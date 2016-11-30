1cb497290a58dc919aa47026e46d5f7464e3b67f
Sergii Bubalo <sergii.bubalo@gmail.com>
2016-11-30 16:54:29 +0200

    Travis error fixing

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         $this->visit('/welcome')
-             ->see('Laravel12');
+             ->see('Larave');
     }
 }
